b2b9b31bb980c58ca4b1c064689092a7ad2d9d53
Vladislav Protchenko <vlad.freezzy@gmail.com>
2025-05-01 17:33:50 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Eureka service
+Emergency notification system eureka service 
---
+++ src/main/java/io/vladprotchenko/enseurekaservice/EnsEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package io.vladprotchenko.enseurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsEurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsEurekaServiceApplication.class, args);
+    }
+
+}
